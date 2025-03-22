185ec930202698b4d55180f1c3518a7d750c4f88
Hammad Afzal <hammad.afzal.code@gmail.com>
2025-03-22 11:59:10 +0500

    basic typsscript code

---
+++ 01/intro.js
@@ -0,0 +1,2 @@
+var user = { name: "hammad", age: 20 };
+console.log(user.name);
---
+++ 01/variableMe.ts
@@ -0,0 +1,5 @@
+let greetings: string = "Hello, World!";
+console.log(greetings);
+
+export {}; // this will temporarily fix the error
+// Error: Cannot redeclare block-scoped variable 'greetings'.
