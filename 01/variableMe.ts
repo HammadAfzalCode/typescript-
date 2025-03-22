c1e5b80ccf1ea65b4900653cc4adf6853b11d724
Hammad Afzal <hammad.afzal.code@gmail.com>
2025-03-22 12:23:46 +0500

    Any and boolean and number

--- 01/variableMe.ts
+++ 01/variableMe.ts
@@ -3,3 +3,10 @@ console.log(greetings);
 
 export {}; // this will temporarily fix the error
 // Error: Cannot redeclare block-scoped variable 'greetings'.
+
+
+let userId: number = 1; // here explicitly defining the type of variable
+let userName = "hammad"; // here type is inferred by the value assigned to the variable
+// userName = 1; // Error: Type '1' is not assignable to type 'string'.
+// if we assigning a value right away then we can skip defining the type of variable
+
